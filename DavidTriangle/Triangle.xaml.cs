57a59d64d9b03bb964c3ccd8566aa6b0f380af82
zhuliang <zhuliang@chukong-inc.com>
2015-03-24 12:36:40 +0800

    添加刻度

--- DavidTriangle/Triangle.xaml.cs
+++ DavidTriangle/Triangle.xaml.cs
@@ -22,6 +22,59 @@ namespace DavidTriangle
         public Triangle()
         {
             InitializeComponent();
+            InitiRuler();
+        }
+
+        double triangleHeight = 270.0;
+        double triangleWidth = 300.0;
+        /// <summary>
+        /// 初始化标尺
+        /// </summary>
+        private void InitiRuler()
+        {
+            double h = triangleHeight;
+            double w = triangleWidth;
+            SolidColorBrush brush  = new SolidColorBrush(Colors.Black);
+            int rulerNumber = 10;
+            for (int i = 1; i < 10; i++)
+            {
+               
+                double x =  (w * i / 20);
+                double y = h * i / 10;
+                Line lineCH4 = new Line();
+                lineCH4.Stroke = brush;
+                lineCH4.X1 = (w / 2) - x;
+                lineCH4.Y1 = y;
+                lineCH4.X2 = (w / 2) - x + 5;
+                lineCH4.Y2 = y;
+                if (i % 2 == 0)
+                {
+                    rulerNumber =100- rulerNumber * i;
+                    TextBlock txt = new TextBlock();
+                    txt.Text = rulerNumber.ToString();
+                    this.rootCanvas.Children.Add(txt);
+                    Canvas.SetLeft(txt, lineCH4.X1 - 10 - 10);
+                    Canvas.SetTop(txt, y - 10);
+                }
+                this.rootCanvas.Children.Add(lineCH4);
+
+                Line lineC2H4 = new Line();
+                lineC2H4.Stroke = brush;
+                lineC2H4.X2 = (w / 2) + x;
+                lineC2H4.Y2 = y;
+                lineC2H4.X1 = (w / 2) + x - 5;
+                lineC2H4.Y1 = y + 3;
+                this.rootCanvas.Children.Add(lineC2H4);
+
+                Line lineC2H2 = new Line();
+                lineC2H2.Stroke = brush;
+                lineC2H2.X1 = w * i / 10;
+                lineC2H2.Y1 = h;
+                lineC2H2.X2 = lineC2H2.X1 + 3;
+                lineC2H2.Y2 = lineC2H2.Y1 - 5;
+                this.rootCanvas.Children.Add(lineC2H2);
+                rulerNumber = 10;
+            }
         }
     }
 }
