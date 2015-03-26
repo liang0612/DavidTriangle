a3deddfc3ad2b3a2c1b5913c6d5f0a03bf02288c
zhuliang <zhuliang@chukong-inc.com>
2015-03-27 00:40:20 +0800

    绘制相关区域

--- DavidTriangle/Triangle.xaml.cs
+++ DavidTriangle/Triangle.xaml.cs
@@ -22,14 +22,15 @@ namespace DavidTriangle
         public Triangle()
         {
             InitializeComponent();
-            DrawingPoint();
+            //DrawingPoint();
             InitiRuler();
 
         }
 
-        public static double triangleHeight = 270.0;
-        public static double triangleWidth = 300.0;
-
+        //public static double triangleHeight = 270.0;
+        //public static double triangleWidth = 300.0;
+        public static double triangleHeight = 445;
+        public static double triangleWidth = 510;
         /// <summary>
         /// 初始化标尺
         /// </summary>
@@ -81,32 +82,65 @@ namespace DavidTriangle
         }
         private void DrawingPoint()
         {
+            this.rootCanvas.Children.Add(DrawingD1());
+            this.rootCanvas.Children.Add(DrawingD2());
+        }
+
+        private Path DrawingD1()
+        {
             SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(167, 202, 240));
             Path path = new Path();
-            path.StrokeThickness = 0; 
+            path.StrokeThickness = 0;
             //path.Opacity = 0.8;
             path.Fill = brush;
             path.Stroke = brush;
             PathGeometry geo = new PathGeometry();
             PathFigure figure = new PathFigure();
-            figure.StartPoint = CoordinateConverterHelp.CoordinateConverter(87, EnumGas.CH4);
+            figure.StartPoint = new Point(220,60);
             figure.IsClosed = false;
-            double lineX = CoordinateConverterHelp.CoordinateConverter(45, EnumGas.C2H2).X;
-            double lineY = CoordinateConverterHelp.CoordinateConverter(65, EnumGas.CH4).Y;
-            LineSegment line = new LineSegment(new Point(lineX,lineY),false);
-
-            double line1X = CoordinateConverterHelp.CoordinateConverter(78, EnumGas.C2H2).X;
-            double line1Y = CoordinateConverterHelp.CoordinateConverter(0, EnumGas.CH4).Y;
-            LineSegment line1 = new LineSegment(new Point(line1X, line1Y), false);
-
-            LineSegment line2 = new LineSegment(new Point(0, 270), false);
-
+            //double lineX = CoordinateConverterHelp.CoordinateConverter(45, EnumGas.C2H2).X;
+            //double lineY = CoordinateConverterHelp.CoordinateConverter(67, EnumGas.CH4).Y;
+            LineSegment line = new LineSegment(new Point(279, 159), false);
+            //double line1X = CoordinateConverterHelp.CoordinateConverter(78, EnumGas.C2H2).X;
+            //double line1Y = CoordinateConverterHelp.CoordinateConverter(0, EnumGas.CH4).Y;
+            LineSegment line1 = new LineSegment(new Point(120, 444), false);
+            LineSegment line2 = new LineSegment(new Point(0, 444), false);
             figure.Segments.Add(line);
             figure.Segments.Add(line1);
             figure.Segments.Add(line2);
             geo.Figures.Add(figure);
             path.Data = geo;
-            this.rootCanvas.Children.Add(path);
+            return path;
+        }
+
+        private Path DrawingD2()
+        {
+            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(0, 255, 255));
+            Path path = new Path();
+            path.StrokeThickness = 0;
+            //path.Opacity = 0.8;
+            path.Fill = brush;
+            path.Stroke = brush;
+            PathGeometry geo = new PathGeometry();
+            PathFigure figure = new PathFigure();
+            figure.StartPoint = new Point(279,159);
+            figure.IsClosed = true;
+    
+            LineSegment line = new LineSegment(new Point(326, 235), false);
+
+            LineSegment line1 = new LineSegment(new Point(284, 306), false);
+
+            LineSegment line2 = new LineSegment(new Point(364, 444), false);
+
+            LineSegment line3 = new LineSegment(new Point(121, 444), false);
+            figure.Segments.Add(line);
+            figure.Segments.Add(line1);
+            figure.Segments.Add(line2);
+            figure.Segments.Add(line3);
+            geo.Figures.Add(figure);
+
+            path.Data = geo;
+            return path;
         }
 
     }
