e33fa7dffe3762574eb6b57b715f5ce927721f4d
zhuliang <zhuliang@chukong-inc.com>
2015-03-27 15:01:39 +0800

    调整区域

--- DavidTriangle/Triangle.xaml.cs
+++ DavidTriangle/Triangle.xaml.cs
@@ -27,10 +27,8 @@ namespace DavidTriangle
 
         }
 
-        //public static double triangleHeight = 270.0;
-        //public static double triangleWidth = 300.0;
-        public static double triangleHeight = 445;
-        public static double triangleWidth = 510;
+        public static double triangleHeight = 443;
+        public static double triangleWidth = 512;
         /// <summary>
         /// 初始化标尺
         /// </summary>
@@ -53,7 +51,7 @@ namespace DavidTriangle
                 lineCH4.Y2 = y;
                 if (i % 2 == 0)
                 {
-                    rulerNumber = 100 - rulerNumber * i;
+                    rulerNumber = 100 - 10 * i;
                     TextBlock txt = new TextBlock();
                     txt.Text = rulerNumber.ToString();
                     this.rootCanvas.Children.Add(txt);
@@ -68,6 +66,16 @@ namespace DavidTriangle
                 lineC2H4.Y2 = y;
                 lineC2H4.X1 = (w / 2) + x - 5;
                 lineC2H4.Y1 = y + 3;
+
+                if (i % 2 == 0)
+                {
+                    rulerNumber = 10 * i;
+                    TextBlock txt = new TextBlock();
+                    txt.Text = rulerNumber.ToString();
+                    this.rootCanvas.Children.Add(txt);
+                    Canvas.SetLeft(txt, lineC2H4.X2+5);
+                    Canvas.SetTop(txt, lineC2H4.Y2-10);
+                }
                 this.rootCanvas.Children.Add(lineC2H4);
 
                 Line lineC2H2 = new Line();
@@ -76,71 +84,17 @@ namespace DavidTriangle
                 lineC2H2.Y1 = h;
                 lineC2H2.X2 = lineC2H2.X1 + 3;
                 lineC2H2.Y2 = lineC2H2.Y1 - 5;
+                if (i % 2 == 0)
+                {
+                    rulerNumber =100- 10 * i;
+                    TextBlock txt = new TextBlock();
+                    txt.Text = rulerNumber.ToString();
+                    this.rootCanvas.Children.Add(txt);
+                    Canvas.SetLeft(txt, lineC2H2.X1-5);
+                    Canvas.SetTop(txt, lineC2H2.Y1);
+                }
                 this.rootCanvas.Children.Add(lineC2H2);
-                rulerNumber = 10;
             }
-        }
-        private void DrawingPoint()
-        {
-            this.rootCanvas.Children.Add(DrawingD1());
-            this.rootCanvas.Children.Add(DrawingD2());
-        }
-
-        private Path DrawingD1()
-        {
-            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(167, 202, 240));
-            Path path = new Path();
-            path.StrokeThickness = 0;
-            //path.Opacity = 0.8;
-            path.Fill = brush;
-            path.Stroke = brush;
-            PathGeometry geo = new PathGeometry();
-            PathFigure figure = new PathFigure();
-            figure.StartPoint = new Point(220,60);
-            figure.IsClosed = false;
-            //double lineX = CoordinateConverterHelp.CoordinateConverter(45, EnumGas.C2H2).X;
-            //double lineY = CoordinateConverterHelp.CoordinateConverter(67, EnumGas.CH4).Y;
-            LineSegment line = new LineSegment(new Point(279, 159), false);
-            //double line1X = CoordinateConverterHelp.CoordinateConverter(78, EnumGas.C2H2).X;
-            //double line1Y = CoordinateConverterHelp.CoordinateConverter(0, EnumGas.CH4).Y;
-            LineSegment line1 = new LineSegment(new Point(120, 444), false);
-            LineSegment line2 = new LineSegment(new Point(0, 444), false);
-            figure.Segments.Add(line);
-            figure.Segments.Add(line1);
-            figure.Segments.Add(line2);
-            geo.Figures.Add(figure);
-            path.Data = geo;
-            return path;
-        }
-
-        private Path DrawingD2()
-        {
-            SolidColorBrush brush = new SolidColorBrush(Color.FromRgb(0, 255, 255));
-            Path path = new Path();
-            path.StrokeThickness = 0;
-            //path.Opacity = 0.8;
-            path.Fill = brush;
-            path.Stroke = brush;
-            PathGeometry geo = new PathGeometry();
-            PathFigure figure = new PathFigure();
-            figure.StartPoint = new Point(279,159);
-            figure.IsClosed = true;
-    
-            LineSegment line = new LineSegment(new Point(326, 235), false);
-
-            LineSegment line1 = new LineSegment(new Point(284, 306), false);
-
-            LineSegment line2 = new LineSegment(new Point(364, 444), false);
-
-            LineSegment line3 = new LineSegment(new Point(121, 444), false);
-            figure.Segments.Add(line);
-            figure.Segments.Add(line1);
-            figure.Segments.Add(line2);
-            figure.Segments.Add(line3);
-            geo.Figures.Add(figure);
-
-            path.Data = geo;
-            return path;
         }
 
     }
